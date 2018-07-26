e79a9c617f2872bddeea3448da040a1949126fcc
Anbarasu Angappan <anburvhs@hotmail.com>
2018-07-26 19:43:56 +0530

    Add project files.

---
+++ WindowsServiceHost/HelloWindowsService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceHost
+{
+    public partial class HelloWindowsService : ServiceBase
+    {
+        ServiceHost serviceHost;
+        public HelloWindowsService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            serviceHost = new ServiceHost(typeof(HelloService.HelloService));
+            serviceHost.Open();
+
+
+        }
+
+        protected override void OnStop()
+        {
+            serviceHost.Close();
+        }
+    }
+}
